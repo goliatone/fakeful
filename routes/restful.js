c26b9cf790d45be3d3620af33d674daead30c1e6
goliatone <burgosemi@gmail.com>
2014-07-10 23:01:20 -0400

    Routes: Updated restful routes

--- routes/restful.js
+++ routes/restful.js
@@ -1,24 +1,43 @@
+var db = require('../libs/filedb');
+
 var routes = {};
 
+// GET /:resource
 routes.list = function(req, res, next) {
-
+    // res.send('list=> resource:' + req.params.resource);
+    db(req.params.resource).get(function(err, resources) {
+        res.jsonp(resources);
+    });
 };
-
+// POST /:resource
 routes.create = function(req, res, next) {
-
+    db(req.params.resource).put(req.body, function(err, resource) {
+        res.jsonp(resource);
+    });
 };
 
+// GET /:resource/:id
 routes.read = function(req, res, next) {
+    // res.send('read=> resource:' + req.params.resource + ' id: ' + req.params.id);
 
+    db(req.params.resource).getSingle(+req.params.id, function(err, resource) {
+        res.jsonp(resource);
+    });
 };
 
+// PUT /:resource/:id
+// PATCH /:resource/:id
 routes.update = function(req, res, next) {
-
+    db(req.params.resource).put(req.body, function(err, resource) {
+        res.jsonp(resource);
+    });
 };
 
 // DELETE /:resource/:id
 routes.destroy = function(req, res, next) {
-
+    db(req.params.resource).del(req.params.id, function(err, resource) {
+        res.jsonp(resource);
+    });
 };
 
 
