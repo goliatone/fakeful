cbbeb30ea4528f8022eade8dd42c613eb2648b88
goliatone <burgosemi@gmail.com>
2014-07-10 23:00:34 -0400

    Server: Added error handler middleware

--- libs/server.js
+++ libs/server.js
@@ -19,6 +19,21 @@ server.use(express.static(__dirname + '/public/'));
 server.use(express.directory(__dirname + '/public/'));
 server.use(server.router);
 
+server.use(error);
+
+// error handling middleware have an arity of 4
+// instead of the typical (req, res, next),
+// otherwise they behave exactly like regular
+// middleware, you may have several of them,
+// in different orders etc.
+function error(err, req, res, next) {
+    // log it
+    console.error(err.stack);
+
+    // respond with 500 "Internal Server Error".
+    res.send(500);
+}
+
 server.findPort = function getPort() {
     var port = process.env.PORT || 9390;
     var args = process.argv.splice(2);
