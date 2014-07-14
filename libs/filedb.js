3c6f1c173fe554ff5ef1ffba2ca817b5c02354bb
goliatone <burgosemi@gmail.com>
2014-07-14 15:06:59 -0400

    FileDB: Update structure, make API similar to MongoDB

--- libs/filedb.js
+++ libs/filedb.js
@@ -1,144 +1,133 @@
 var fs = require('fs'),
     _ = require('underscore');
 
-/**
- * Creates a DB instance that is a simple wapper around the given file.
- * @param file File path
- * @param idAttribute Name of the attribut to use as the id. Used to identify
- * an existing document by the put and delete methods. Default value is 'id'
- */
-var open = function(file, idAttribute) {
+
+var JsonDB = function(file, idAttribute) {
     var idAttr = idAttribute || '_index';
     var file = file.indexOf('.json') === -1 ? file + '.json' : file;
     if (!file) {
         throw new Error("DB: file name is required");
     }
 
-    var convertValueToFilter = function(attrs) {
-        // if a value was provided instead of an object, use the value as an id
-        if (attrs !== undefined && !_.isObject(attrs)) {
-            var val = attrs;
-            attrs = {};
-            attrs[idAttr] = val;
-        }
-        return attrs;
-    };
+    var db = new FileDB(file, {
+        idAttribute: idAttr
+    });
 
-
-    var get = function(attrs, cb) {
-
-        //predicate is optional
-        if (_.isFunction(attrs)) {
-            cb = attrs;
-            attrs = undefined;
-        }
-        if (!fs.existsSync(file)) {
-            cb(null, []);
-            return;
-        }
-
-        fs.readFile(file, 'utf-8', function(err, data) {
-            if (err) {
-                cb(err);
-                return;
-            }
-
-            if (!data || data === '') {
-                cb(null, []);
-                return;
-            }
-
-            try {
-                var docs = JSON.parse(data);
-            } catch (err) {
-                cb(err);
-                return;
-            }
-
-            attrs = convertValueToFilter(attrs);
-
-            if (attrs) {
-                docs = _.where(docs, attrs);
-            }
-            cb(null, docs);
-        });
-
-    };
-
-
-    var getSingle = function(attrs, cb) {
-
-        get(attrs, function(err, data) {
-
-            if (err) {
-                cb(err, null);
-                return;
-            }
-            var selected = (data && data.length > 0) ? data[0] : undefined;
-            cb(null, selected);
-        });
-
-    };
-
-
-    var put = function(newDoc, cb) {
-
-        get(function(err, docs) {
-
-            if (err) {
-                cb(err);
-                return;
-            }
-
-            var match = _.filter(docs, function(doc) {
-                return doc[idAttr] === newDoc[idAttr];
-            });
-            if (match.length >= 1) {
-                _.extend(match[0], newDoc);
-            } else {
-                newDoc[idAttr] = docs.push(newDoc) - 1;
-            }
-            _save(docs);
-            cb(null, docs);
-        });
-    };
-
-    var _save = function(docs) {
-        fs.writeFile(file, JSON.stringify(docs, null, " "), 'utf-8', cb);
-    };
-
-
-    var del = function(attrs, cb) {
-        if (!fs.existsSync(file)) {
-            cb(null);
-            return;
-        }
-
-        get(function(err, docs) {
-
-            if (err) {
-                cb(err);
-                return;
-            }
-
-            attrs = convertValueToFilter(attrs);
-
-            var toDelete = _.where(docs, attrs);
-            var toKeep = _.difference(docs, toDelete);
-
-            _save(toKeep);
-            cb(null, toKeep);
-        });
-
-    };
-
-    return {
-        get: get,
-        getSingle: getSingle,
-        put: put,
-        "delete": del
-    }
-
+    return db;
 };
 
-module.exports = open;
+var _convertValueToFilter = function(attrs, idAttr) {
+    // if a value was provided instead of an object, use the value as an id
+    if (attrs !== undefined && !_.isObject(attrs)) {
+        var val = attrs;
+        attrs = {};
+        attrs[idAttr] = val;
+    }
+    return attrs;
+};
+
+var _save = function(file, docs) {
+    fs.writeFile(file, JSON.stringify(docs, null, " "), 'utf-8', cb);
+};
+
+var _load = function(file, callback) {
+    fs.readFile(file, 'utf-8', callback);
+};
+
+var FileDB = function(file, config) {
+    this.file = file;
+    this.idAttr = config.idAttr || '_index';
+};
+
+FileDB.prototype.find = function(attrs, cb) {
+    //predicate is optional
+    if (_.isFunction(attrs)) {
+        cb = attrs;
+        attrs = undefined;
+    }
+
+    if (!fs.existsSync(this.file)) {
+        console.warn('FILE DOES NOT EXIST', this.file);
+        cb(null, []);
+        return;
+    }
+
+    _load(this.file, (function(err, data) {
+
+        if (err) return cb(err);
+
+        if (!data || data === '') return cb(null, []);
+
+        try {
+            var docs = JSON.parse(data);
+
+        } catch (err) {
+            cb(err);
+
+            return;
+        }
+
+        attrs = _convertValueToFilter(attrs, this.idAttr);
+
+        if (attrs) docs = _.where(docs, attrs);
+
+        cb(null, docs);
+    }).bind(this));
+};
+
+FileDB.prototype.findOne = function(attrs, cb) {
+    this.find(attrs, (function(err, data) {
+        if (err) {
+            cb(err, null);
+            return;
+        }
+        var selected = (data && data.length > 0) ? data[0] : undefined;
+        cb(null, selected);
+    }).bind(this));
+};
+
+FileDB.prototype.insert = function(newDoc, cb) {
+    this.find((function(err, docs) {
+
+        if (err) {
+            cb(err);
+            return;
+        }
+
+        var match = _.filter(docs, function(doc) {
+            return doc[this.idAttr] === newDoc[this.idAttr];
+        });
+        if (match.length >= 1) {
+            _.extend(match[0], newDoc);
+        } else {
+            newDoc[this.idAttr] = docs.push(newDoc) - 1;
+        }
+        _save(this.file, docs);
+        cb(null, docs);
+    }).bind(this));
+};
+
+FileDB.prototype.remove = function(attrs, cb) {
+    if (!fs.existsSync(this.file)) {
+        cb(null);
+        return;
+    }
+
+    this.find((function(err, docs) {
+
+        if (err) {
+            cb(err);
+            return;
+        }
+
+        attrs = _convertValueToFilter(attrs);
+
+        var toDelete = _.where(docs, attrs);
+        var toKeep = _.difference(docs, toDelete);
+
+        _save(this.file, toKeep);
+        cb(null, toKeep);
+    }).bind(this));
+};
+
+module.exports = JsonDB;
