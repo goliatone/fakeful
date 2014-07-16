a423726441744973880bf59b7db3bdc8e28299ee
goliatone <burgosemi@gmail.com>
2014-07-16 16:48:11 -0400

    Convert json2json, WIP

--- libs/json2json.js
+++ libs/json2json.js
@@ -1,27 +1,39 @@
-var fs = require('fs'),
-    JSONStream = require('JSONStream'),
+var fs = require('fs');
 
-var getStream(jsonData) = function() {
-    var stream = fs.createReadStream(jsonData, {
-        encoding: 'utf8'
-    });
 
-    var parser = JSONStream.parse();
 
-    return stream.pipe(parser);
+var json2json = function(config, callback) {
+    if (!config.input) {
+        console.error("You miss a input file");
+        process.exit(1);
+    }
+
+    var cv = new CV(config, callback);
 };
 
-getStream(options.input).on('complete', function(err) {
-    // handle any errors
-});
+function CV(config, callback) {
+    this.config = config;
+    this.config.headerTransforms = config.headerTransforms || [];
+    var data = this.load_file(config.input);
+    data = JSON.parse(data);
+    this.makeJSON(data, config.output, callback)
+}
 
-var json2json = function(options, callback) {
-    var output = require(options.input);
-    var stream = fs.createWriteStream(output, {
-        flags: 'w'
-    });
-    stream.write(JSON.stringify(output));
-    callback(null, output);
+CV.prototype.load_file = function(input) {
+    return fs.readFileSync(input, "utf8");
+};
+
+CV.prototype.makeJSON = function(data, path, callback) {
+    if (path !== null) {
+        var stream = fs.createWriteStream(path, {
+            flags: 'w'
+        });
+        var write = (typeof data !== 'string') ? JSON.stringify(data) : data;
+        stream.write(write);
+        callback(null, data);
+    } else {
+        callback(null, data);
+    }
 };
 
 module.exports = json2json;
