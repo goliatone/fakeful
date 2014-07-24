62f5cb9a4d3134b076e3f0617663abad8c4c340f
goliatone <burgosemi@gmail.com>
2014-07-24 11:34:26 -0400

    Fixtures: test, use template file

--- fixtures/test.js
+++ fixtures/test.js
@@ -1,10 +1,9 @@
 var fakeit = require('./index');
 var fs = require('fs');
 
-var template = '[{{#repeat 22}}{ "name": "{{name}}", "address": "{{streetAddress}}"}{{/repeat}}]';
-
-template = fs.readFileSync('./users.jtpl', "utf8");
+var template = '[{{#repeat 22}}{ "name": "{{findName}}", "handle":"{{userName}}", "address": "{{streetAddress}}", "phone":"{{phoneNumber}}", "company":"{{companyName}}", "img":"{{avatar}}", "bio": "{{sentences 3}}" }{{/repeat}}]';
+template = fs.readFileSync('./fixtures/users.jtpl', "utf8");
 var result = fakeit.parse(template);
-var out = JSON.parse(result);
+// var out = JSON.parse(result);
 
 console.log(result);
