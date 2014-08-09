e85f8f385e32f360efb0c4989cae848dc24d549a
goliatone <burgosemi@gmail.com>
2014-08-09 01:12:44 -0400

    index.js: Added routesPath option

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var server = require('./libs/server');
 // server.use(server.express.static(__dirname + '/public/'));
 // server.use(server.express.directory(__dirname + '/public/'));
 server.run({
-    dirname: __dirname + '/public'
+    dirname: __dirname + '/public',
+    routesPath: __dirname + '/routes'
 });
